43978d5d9e9b107c2cf58b4b6987f303bf16f400
Mark <mark@remarkablemark.org>
2020-04-30 19:52:54 -0400

    refactor(texsvg): change `texsvg` to async function

--- src/index.ts
+++ src/index.ts
@@ -11,24 +11,23 @@ const config = {
 /**
  * Converts TeX expression to SVG markup.
  */
-const texsvg = (tex: string): Promise<string> => {
+async function texsvg(tex: string): Promise<string> {
   if (typeof tex !== 'string') {
-    return Promise.reject('First argument must be a string');
+    throw 'First argument must be a string';
   }
 
   if (tex2svg) {
-    return Promise.resolve(tex2svg(tex));
+    return tex2svg(tex);
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return mathjax.init(config).then((MathJax: any) => {
-    const { adaptor } = MathJax.startup;
-    tex2svg = (tex: string): string => {
-      return adaptor.innerHTML(MathJax.tex2svg(tex));
-    };
-    return tex2svg(tex);
-  });
-};
+  const MathJax = await mathjax.init(config);
+  const { adaptor } = MathJax.startup;
+
+  tex2svg = (tex: string): string => adaptor.innerHTML(MathJax.tex2svg(tex));
+
+  const svg = tex2svg(tex);
+  return svg;
+}
 
 texsvg.default = texsvg;
 
