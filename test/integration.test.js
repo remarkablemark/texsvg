b91003469f57c4f5af3f29833aa8c5dcda85c9a8
Mark <mark@remarkablemark.org>
2020-04-30 20:13:59 -0400

    test(integration): verify default property

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,7 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const texsvg = require('../cjs/index.js');
 
-it('renders TeX to SVG correctly', async () => {
-  const tex = '\\frac{a}{b}';
-  expect(await texsvg(tex)).toMatchSnapshot();
+describe('texsvg', () => {
+  it('can be required using CommonJS or imported using ES Modules', () => {
+    expect(texsvg).toBe(texsvg.default);
+  });
+
+  it('renders TeX to SVG correctly', async () => {
+    const tex = '\\frac{a}{b}';
+    expect(await texsvg(tex)).toMatchSnapshot();
+  });
 });
