f9cdbc08b5b6096c07935aaec6ef48451c373ba8
Mark <mark@remarkablemark.org>
2025-08-04 14:22:57 -0400

    test(jest): fix errors

--- jest.config.integration.ts
+++ jest.config.integration.ts
@@ -1,7 +1,12 @@
 import type { Config } from 'jest';
 
 const config: Config = {
+  moduleFileExtensions: ['js', 'ts'],
   preset: 'ts-jest',
+  reporters:
+    process.env.CI === 'true'
+      ? [['github-actions', { silent: false }], 'summary']
+      : undefined,
   rootDir: 'test',
   testEnvironment: 'node',
 };
